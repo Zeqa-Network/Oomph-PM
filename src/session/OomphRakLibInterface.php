821b750deece97ae5602acdcf0b9dceacfb3b8b3
Wraith0x10 <haydenxp123@gmail.com>
2024-07-29 19:22:18 +0100

    oomph: use current protocol

--- src/session/OomphRakLibInterface.php
+++ src/session/OomphRakLibInterface.php
@@ -7,6 +7,7 @@ use ethaniccc\Oomph\utils\ReflectionUtils;
 use pocketmine\network\mcpe\compression\ZlibCompressor;
 use pocketmine\network\mcpe\convert\TypeConverter;
 use pocketmine\network\mcpe\protocol\PacketPool;
+use pocketmine\network\mcpe\protocol\ProtocolInfo;
 use pocketmine\network\mcpe\raklib\RakLibInterface;
 use pocketmine\network\mcpe\raklib\RakLibPacketSender;
 use pocketmine\network\mcpe\StandardEntityEventBroadcaster;
@@ -17,7 +18,7 @@ use ReflectionException;
 class OomphRakLibInterface extends RakLibInterface{
 	public function __construct(Server $server, string $ip, int $port, bool $ipV6){
 		$typeConverter = TypeConverter::getInstance();
-		$packetBroadcaster = new StandardPacketBroadcaster($server);
+		$packetBroadcaster = new StandardPacketBroadcaster($server, ProtocolInfo::CURRENT_PROTOCOL);
 		$entityEventBroadcaster = new StandardEntityEventBroadcaster($packetBroadcaster, $typeConverter);
 		parent::__construct($server, $ip, $port, $ipV6, $packetBroadcaster, $entityEventBroadcaster, $typeConverter);
 	}
